1c04d260823b6b064d79adda1c3915ab55b55fa1
Taras Oleksyshyn <x3mal18@gmail.com>
2022-06-04 08:50:08 +0300

    mailer was added

--- src/js/script.js
+++ src/js/script.js
@@ -15,14 +15,14 @@ $(document).ready(function () {
     ]
   });
 
-  // tabs script
+  //-------- tabs script
   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
 
-  // catalog item descr toggle
+  //-------- catalog item descr toggle
   function toggleSlide(item) {
     $(item).each(function (i) {
       $(this).on('click', function (e) {
@@ -52,7 +52,7 @@ $(document).ready(function () {
     });
   });
 
-  // form validation
+  //-------- form validation
   function validateForms(form) {
     $(form).validate({
       rules: {
@@ -81,7 +81,44 @@ $(document).ready(function () {
   validateForms('#consultation form');
   validateForms('#order form');
 
-  // phone input masked
+  //-------- phone input masked
   $('input[name=phone]').mask("(999) 999-9999", { placeholder: "Ваш телефон" });
+
+  //-------- work with forms
+  $('form').submit(function (e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function () {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  })
+
+  // Smooth scroll and pageup
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1200) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href=#up]").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+  });
 
 });
